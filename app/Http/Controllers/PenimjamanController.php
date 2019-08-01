4727157f72b48a21e9fb39bfd5327f3381175225
desi lastasari <desilastasari@gmail.com>
2019-08-01 09:31:19 +0700

    connect

---
+++ app/Http/Controllers/PenimjamanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\penimjaman;
+use Illuminate\Http\Request;
+
+class PenimjamanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\penimjaman  $penimjaman
+     * @return \Illuminate\Http\Response
+     */
+    public function show(penimjaman $penimjaman)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\penimjaman  $penimjaman
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(penimjaman $penimjaman)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\penimjaman  $penimjaman
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, penimjaman $penimjaman)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\penimjaman  $penimjaman
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(penimjaman $penimjaman)
+    {
+        //
+    }
+}
---
+++ database/migrations/2019_08_01_011841_create_peminjamen_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeminjamenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peminjamen', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedbigInteger('petugas_id');
+            $table->unsignedbigInteger('peminjams_id');
+            $table->date('pjmn_tgl');
+            $table->date('pjmn_tgl_kembali');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peminjamen');
+    }
+}
